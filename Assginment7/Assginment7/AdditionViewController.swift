8dd838ead845033da4eca440e9c00314eec44d55
S-maruyama0223 <wanshanxiangtailang0223@gmail.com>
2020-11-02 23:34:31 +0900

    afterreview

--- Assginment7/Assginment7/AdditionViewController.swift
+++ Assginment7/Assginment7/AdditionViewController.swift
@@ -9,30 +9,32 @@ import UIKit
 
 
 class AdditionViewController: UIViewController {
-    @IBOutlet weak var leftField: UITextField!
-    @IBOutlet weak var rightField: UITextField!
-    @IBOutlet weak var resultLabel: UILabel!
-    
+    // private付けましょう
+    @IBOutlet private weak var leftField: UITextField!
+    @IBOutlet private weak var rightField: UITextField!
+    @IBOutlet private weak var resultLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         leftField.keyboardType = .numberPad
         rightField.keyboardType = .numberPad
-        
     }
 
     @IBAction func calculate(_ sender: Any) {
-        
         let leftNum = Double(leftField.text ?? "0.0") ?? 0.0
         let rightNum = Double(rightField.text ?? "0.0") ?? 0.0
-        let doAddition = {(num1:Double, num2:Double) ->Double in num1 + num2}
-        
-        func updateLabel(result:Double){
-            resultLabel.text = String(result)
-        }
-        
-        updateLabel(result: doAddition(leftNum, rightNum))
-        
+
+//        let doAddition = {(num1:Double, num2:Double) ->Double in num1 + num2}
+//
+//        func updateLabel(result:Double){
+//            resultLabel.text = String(result)
+//        }
+//
+//        updateLabel(result: doAddition(leftNum, rightNum))
+
+        // 普通にこれで良いかなと、と個人的には思いました
+        resultLabel.text = String(leftNum + rightNum)
+
         view.endEditing(true)
     }
 }
-
