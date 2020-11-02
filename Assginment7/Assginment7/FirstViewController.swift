5f66919ec6e350f0d992d3931c92559db45f691c
S-maruyama0223 <wanshanxiangtailang0223@gmail.com>
2020-11-02 22:42:26 +0900

    完了

---
+++ Assginment7/Assginment7/FirstViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Assginment7
+//
+//  Created by 丸山翔太郎 on 2020/11/02.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    @IBOutlet weak var leftField: UITextField!
+    @IBOutlet weak var rightField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        leftField.keyboardType = .numberPad
+        rightField.keyboardType = .numberPad
+        
+    }
+
+    @IBAction func calculate(_ sender: Any) {
+        
+        let leftNum = Double(leftField.text ?? "0.0") ?? 0.0
+        let rightNum = Double(rightField.text ?? "0.0") ?? 0.0
+        let doAddition = {(num1:Double, num2:Double) ->Double in num1 + num2}
+        
+        func updateLabel(result:Double){
+            resultLabel.text = String(result)
+        }
+        
+        updateLabel(result: doAddition(leftNum, rightNum))
+        
+        view.endEditing(true)
+    }
+}
+
